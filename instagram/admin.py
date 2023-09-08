56665c8fd0e75612b863501f2cc5ab51f3ddd2b9
tutorialcreation <tutorialcreation81@gmail.com>
2023-09-08 12:18:21 +0300

    adding thread to admin

--- instagram/admin.py
+++ instagram/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 
-from .models import Account, Photo, StatusCheck, Video
+from .models import Account, Photo, StatusCheck, Thread, Video
 
 admin.site.register(Photo)
 admin.site.register(Video)
@@ -21,4 +21,12 @@ class StatusAdmin(admin.ModelAdmin):
     def get_form(self, request, obj=None, **kwargs):
         self.exclude = ("id",)
         form = super(StatusAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ("id",)
+        form = super(ThreadAdmin, self).get_form(request, obj, **kwargs)
         return form
