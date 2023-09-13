e714fda2056fb861145b40e127c7b4033c1eb25b
tutorialcreation <tutorialcreation81@gmail.com>
2023-09-13 10:45:29 +0300

    convert to list

--- instagram/tasks.py
+++ instagram/tasks.py
@@ -30,11 +30,19 @@ def send_message(message, thread_id=None, user_id=None, username=None, thread=Tr
             account = Account.objects.get(igname=username)
         except Exception as error:
             print(error)
-        message = cl.direct_send(message, user_ids=[user_id])
-        thread = Thread()
-        thread.thread_id = message.thread_id
-        thread.account = account
-        thread.save()
+        if type(user_id) == list:
+
+            message = cl.direct_send(message, user_ids=user_id)
+            thread = Thread()
+            thread.thread_id = message.thread_id
+            thread.account = account
+            thread.save()
+        elif type(user_id) == str:
+            message = cl.direct_send(message, user_ids=[user_id])
+            thread = Thread()
+            thread.thread_id = message.thread_id
+            thread.account = account
+            thread.save()
 
     if thread_id and thread:
         if type(thread_id) == list:
