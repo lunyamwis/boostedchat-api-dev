b5b622dc81c98f4394764de696a64f318241a10e
tutorialcreation <tutorialcreation81@gmail.com>
2023-09-14 13:51:04 +0300

    removing gunicorn workers

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,6 @@ celery -A setup beat --loglevel=info &
 
 sleep 5
 echo "<<<<<<<<<<<<<<<<<<<< START API >>>>>>>>>>>>>>>>>>>>>>>>"
-# python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
 # Start the API with gunicorn
-gunicorn --bind 0.0.0.0:8000 setup.wsgi --reload --access-logfile '-' --workers=2
+# gunicorn --bind 0.0.0.0:8000 setup.wsgi --reload --access-logfile '-' --workers=2
