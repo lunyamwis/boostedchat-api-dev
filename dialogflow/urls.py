7b0f1736da16af4a6a163947b612cccf66323bad
Darwin <darwinochumi38@gmail.com>
2023-11-28 16:42:39 +0300

    Remove unnecessary url

--- dialogflow/urls.py
+++ dialogflow/urls.py
@@ -5,6 +5,5 @@ from instagram.views import initialize_db
 
 urlpatterns = [
     path("fallbackWebhook/", views.FallbackWebhook.as_view()),
-    path("needsAssessmentWebhook/", views.NeedsAssesmentWebhook.as_view()),
     path("init/", initialize_db, name="initialize_db")
 ]
