ce00d485da5265a0af6074f4b330f2b6da479819
tutorialcreation <tutorialcreation81@gmail.com>
2023-12-01 11:39:51 +0300

    added to allowed hosts the ngrok url

--- setup/settings.py
+++ setup/settings.py
@@ -34,10 +34,18 @@ ALLOWED_HOSTS = [
     "3e6a-62-8-92-218.ngrok-free.app",
     "3e6a-62-8-92-218.ngrok-fr",
     "api.booksy.us.boostedchat.com",
-    "ce2d-105-161-11-162.ngrok-free.app"
+    "ce2d-105-161-11-162.ngrok-free.app",
+    "https://ed48-196-105-37-1.ngrok-free.app"
 ]
-CSRF_TRUSTED_ORIGINS = ["https://api.boostedchat.com","https://api.booksy.us.boostedchat.com","https://a69c-105-60-202-188.ngrok-free.app",
-"https://3e6a-62-8-92-218.ngrok-free.app","https://3e6a-62-8-92-218.ngrok-fr","https://ce2d-105-161-11-162.ngrok-free.app"]
+CSRF_TRUSTED_ORIGINS = ["https://api.boostedchat.com",
+"https://api.booksy.us.boostedchat.com",
+"https://a69c-105-60-202-188.ngrok-free.app",
+"https://3e6a-62-8-92-218.ngrok-free.app",
+"https://3e6a-62-8-92-218.ngrok-fr",
+"https://ce2d-105-161-11-162.ngrok-free.app",
+"https://ed48-196-105-37-1.ngrok-free.app"
+
+]
 DIALOGFLOW_BASE_URL = (
     "https://us-central1-dialogflow.googleapis.com/v3beta1/projects/boostedchatapi/locations/us-central1/"
 )
