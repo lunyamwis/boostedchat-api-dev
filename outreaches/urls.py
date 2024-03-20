996d02feef702b22a657cd7522d0e0033bb069d2
Brian Onang'o <surgbc@gmail.com>
2024-03-20 09:31:21 +0300

    Remove admin from outreaches

--- outreaches/urls.py
+++ outreaches/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
 from outreaches.views import PeriodicTaskViewSet
-# from .views import ReschedulePeriodicTasksAPIView
-from . import views
 
 
 router = routers.DefaultRouter()
@@ -10,12 +8,6 @@ router.register(r'periodic-tasks', PeriodicTaskViewSet)
 
 urlpatterns = [
     path('periodic-tasks/types/', PeriodicTaskViewSet.as_view({'get': 'task_types'}), name='task_types'),
-    # path('periodic-tasks/reschedule/', ReschedulePeriodicTasksAPIView.as_view(), name='reschedule_tasks'),
-    # path('create_vm_and_run_docker/', views.CreateVMAndRunDockerAPIView.as_view(), name='create_vm_and_run_docker'),
     path('', include(router.urls)),
-    # path('periodic-tasks/', ReschedulePeriodicTasksAPIView.as_view(), name='list_tasks'),
-
-    # URL pattern for POST requests to reschedule tasks
-    # path('periodic-tasks/reschedule/', ReschedulePeriodicTasksAPIView.as_view(), name='reschedule_tasks'),
 
 ]
