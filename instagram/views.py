4777c30ac572669e6788be83f616cb6581abbcaa
dennis mokaya <anyoksdenn@gmail.com>
2025-01-09 23:29:29 +0300

    add search filter

--- instagram/views.py
+++ instagram/views.py
@@ -176,6 +176,8 @@ class AccountViewSet(viewsets.ModelViewSet):
                 queryset = queryset.filter(status_param=status_param.strip())
                 # print(queryset.first.status_param)
                 # print("After filter",queryset.count())
+        if search_query:# is not None:
+            queryset = queryset.filter(igname__icontains=search_query)
             
         result_page = paginator.paginate_queryset(queryset, request)  # Apply pagination
         
