6f9a65616c20e1242ef8bd769c34f47b2e16ce50
dennis mokaya <anyoksdenn@gmail.com>
2025-01-10 16:40:00 +0300

    merge search filters

--- instagram/views.py
+++ instagram/views.py
@@ -176,7 +176,7 @@ class AccountViewSet(viewsets.ModelViewSet):
                 queryset = queryset.filter(status_param=status_param.strip())
                 # print(queryset.first.status_param)
                 # print("After filter",queryset.count())
-        if search_query: # is not None:
+        if search_query:# is not None:
             queryset = queryset.filter(igname__icontains=search_query)
             
         result_page = paginator.paginate_queryset(queryset, request)  # Apply pagination
