5236e0b82b212dd4659ef79195f4c28936f5b858
dennis mokaya <anyoksdenn@gmail.com>
2025-01-28 09:45:04 +0300

    trigger staging deploy

--- instagram/models.py
+++ instagram/models.py
@@ -136,7 +136,7 @@ class Comment(BaseModel):
     sourceUserId=  models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self) -> str:
-        return self.message if self.message else self.id
+        return self.message if self.message else self.id 
     
 class Like(BaseModel):
     account = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, blank=True)
