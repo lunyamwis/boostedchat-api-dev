98cc14542067c05af85a6dd3d01aa954c95e82f6
lunyamwis <lutherlunyamwi@gmail.com>
2025-01-31 11:11:11 +0300

    working on it

--- instagram/models.py
+++ instagram/models.py
@@ -58,7 +58,7 @@ class Account(BaseModel):
     is_manually_triggered = models.BooleanField(default=False)
     index = models.IntegerField(default=1)
     notes = models.TextField(null=True, blank=True)  # New notes field
-    outreach_time = models.DateTimeField()
+    outreach_time = models.DateTimeField(null=True, blank=True)
     outreach_success = models.BooleanField(default=False)
     
     def __str__(self) -> str:
