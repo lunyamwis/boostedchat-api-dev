11d596dc6598bfee53a94ad3112940289cc68bce
dennis mokaya <anyoksdenn@gmail.com>
2025-02-07 10:17:10 +0300

    remove allauth.middleware

--- setup/settings.py
+++ setup/settings.py
@@ -141,7 +141,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "auditlog.middleware.AuditlogMiddleware",
-    'allauth.account.middleware.AccountMiddleware'
+    # 'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = "setup.urls"
