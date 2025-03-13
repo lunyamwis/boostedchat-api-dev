2272ed0d73ffdb9a945c9b3b195f33a9b03bd648
lunyamwis <lutherlunyamwi@gmail.com>
2025-03-13 13:34:11 +0300

    testing

--- instagram/views.py
+++ instagram/views.py
@@ -1906,12 +1906,13 @@ class DMViewset(viewsets.ModelViewSet):
         
         # Get the start of yesterday's date
         yesterday = timezone.now().date() - timezone.timedelta(days=1)
+        tomorrow = timezone.now().date() + timezone.timedelta(days=1)
         yesterday_start = timezone.make_aware(timezone.datetime.combine(yesterday, timezone.datetime.min.time()))
         unwanted_usernames = UnwantedAccount.objects.values_list('username', flat=True)
 
         # Filter accounts that are qualified and created from yesterday onwards, and exclude accounts that are not wanted
         accounts = Account.objects.filter(
-            Q(qualified=True) & Q(created_at__gte=yesterday_start)
+            Q(qualified=True) & Q(created_at__gte=yesterday_start) & Q(created_at__lte=tomorrow)
         ).exclude(
             status__name="sent_compliment"
         ).exclude(
